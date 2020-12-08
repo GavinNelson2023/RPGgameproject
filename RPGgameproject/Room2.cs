c5b4ad0fc90e72ca236d9ec40e8c8d3b7ba9cad1
GavinNelson2023 <73494029+GavinNelson2023@users.noreply.github.com>
2020-12-08 13:05:14 -0800

    added the big haha's

--- RPGgameproject/Room2.cs
+++ RPGgameproject/Room2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace RPGgameproject
 {
@@ -42,12 +43,13 @@ namespace RPGgameproject
             pbcharacter.SizeMode = PictureBoxSizeMode.StretchImage;
         }
         public bool traps = true;
+        public bool goodnews = false;
+        public bool badnews = false;
         private void lblact1_Click(object sender, EventArgs e)
         {
             if (traps == true)
             {
-                lblresults.Text = "You try to open the door but it is still locked. You might have to disarm the traps in the room.";
-                lblact2.Text = "2. Disarm Traps";
+                lblresults.Text = "You try to open the door but it is still locked. You feel like you have to destroy something in the room inorder to leave.";
             }
             else
             {
@@ -67,19 +69,30 @@ namespace RPGgameproject
                 if(rngnum == 1)
                 {
                     lblresults.Text = "With your ultra large brain you somehow disarm all the traps in the room perfectly. When you finish disarming the last trap you hear a click from the direction of the door.";
+                    lblact2.Text = "";
+                    lblact3.Text = "";
                     traps = false;
                 }
                 else if(rngnum == 2)
                 {
                     lblresults.Text = "";
+                    lblact2.Text = "";
+                    lblact3.Text = "";
+                    traps = false;
                 }
                 else if(rngnum == 3)
                 {
                     lblresults.Text = "";
+                    lblact2.Text = "";
+                    lblact3.Text = "";
+                    traps = false;
                 }
                 else if(rngnum == 4)
                 {
                     lblresults.Text = "";
+                    lblact2.Text = "";
+                    lblact3.Text = "";
+                    traps = false;
                 }
                 else
                 {
@@ -91,6 +104,7 @@ namespace RPGgameproject
                     lblact3.Text = "";
                     lblact4.Text = "";
                     lblhealthnum.Text = "0";
+                    info.health = 0;
                 }
             }
             else
@@ -101,24 +115,112 @@ namespace RPGgameproject
 
         private void lblact3_Click(object sender, EventArgs e)
         {
-
+            if (info.magic >= 3)
+            {
+                if(info.luck >= 4)
+                {
+                    lblresults.Text = "You cast a spell that reveals traps. You now can see all the traps in the room.";
+                    lblact2.Text = "2. Disarm Traps";
+                }
+                else
+                {
+                    lblresults.Text = "You cast a spell to detect danger. You now can see the whole world is dangerous but all the traps in the room are not helping that feeling.";
+                    lblact2.Text = "2. Disarm Traps";
+                }
+                
+            }
+            else
+            {
+                lblresults.Text = "You acidentily steped on a preassure plate and got shot in the knee with a arrow. You suddenly feel compelled to say 'I used to be an adventurer like you. Then I took an arrow in the knee.'";
+                lblact2.Text = "2. Disarm Traps";
+            }
         }
 
         private void lblact4_Click(object sender, EventArgs e)
         {
-
+            if (info.health == 10)
+            {
+                lblresults.Text = "You are already at full health. Please select another option.";
+            }
+            else if (info.health == 9 || info.health == 8)
+            {
+                info.health = 10;
+                lblresults.Text = "You heal to full health.";
+                lblhealthnum.Text = "" + info.health;
+                lblact4.Text = "";
+            }
+            else if (info.health == 7)
+            {
+                info.health = 9;
+                lblresults.Text = "You heal for two points of damage.";
+                lblhealthnum.Text = "" + info.health;
+                lblact4.Text = "";
+            }
+            else if (info.health == 6)
+            {
+                info.health = 8;
+                lblresults.Text = "You heal for two points of damage.";
+                lblhealthnum.Text = "" + info.health;
+                lblact4.Text = "";
+            }
+            else if (info.health == 5)
+            {
+                info.health = 7;
+                lblresults.Text = "You heal for two points of damage.";
+                lblhealthnum.Text = "" + info.health;
+                lblact4.Text = "";
+            }
+            else if (info.health == 4)
+            {
+                info.health = 6;
+                lblresults.Text = "You heal for two points of damage.";
+                lblhealthnum.Text = "" + info.health;
+                lblact4.Text = "";
+            }
+            else if (info.health == 3)
+            {
+                info.health = 5;
+                lblresults.Text = "You heal for two points of damage.";
+                lblhealthnum.Text = "" + info.health;
+                lblact4.Text = "";
+            }
+            else if (info.health == 2)
+            {
+                info.health = 4;
+                lblresults.Text = "You heal for two points of damage.";
+                lblhealthnum.Text = "" + info.health;
+                lblact4.Text = "";
+            }
+            else if (info.health == 1)
+            {
+                info.health = 3;
+                lblresults.Text = "You heal for two points of damage.";
+                lblhealthnum.Text = "" + info.health;
+                lblact4.Text = "";
+            }
+            else
+            {
+                Gameover go = new Gameover();
+                go.Show();
+                this.Hide();
+            }
         }
 
         private void lblsearchoption1_Click(object sender, EventArgs e)
         {
             lblresults.Text = "The good news is you cleared the dungeon.";
             lblsearchoption1.Text = "";
+            
         }
 
         private void lblsearchoption2_Click(object sender, EventArgs e)
         {
-            //lblresults.Text = "The bad news is you are dead, and you took half of the country side with you.";
-            //lblsearchoption2.Text = "";
+            
+            lblresults.Text = "The bad news is you are dead, and you took half of the country side with you.";
+            lblsearchoption2.Text = "";
+            lblsearchoption1.Text = "";
+
+            Thread.Sleep(1000);
             Gameover go = new Gameover();
             go.Show();
             this.Hide();
